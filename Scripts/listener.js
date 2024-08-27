7cbc184ab4878158f595783920a1f0909e26fdcc
InParadise <wpar8157@gmail.com>
2024-08-26 21:39:04 -0500

    Fixed Broke Paths

--- Scripts/listener.js
+++ Scripts/listener.js
@@ -1,8 +1,4 @@
 const aboutButton = document.querySelector('.buttonAbout')
 
-function changePage(){
-    window.location.href = './about.html'
-}
-
 aboutButton.addEventListener('click', () => window.location.href = '../Pages/about.html')
 
